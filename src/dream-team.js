49fa81edb585f2ffba73269a0cde6ab3b1a1c70a
VitalkaMihal <vitalimikhalkevich1986@gmail.com>
2021-03-08 21:20:17 +0300

    feat: add team name

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,11 +1,20 @@
 
 
-module.exports = function createDreamTeam( members ) {
+module.exports = function createDreamTeam(members) {
   let arr =[];
-  for (let i =0; i < members.length; i++){
-     if (typeof members[i] === 'string'){
-     arr.push(members[i][0]);
-   }
- }
-  return arr.join('');
+  if (Array.isArray(members) === false){
+    return false;
+  } else {
+    for (let i =0; i < members.length; i++){
+      if (typeof members[i] === 'string'){
+        for (let j = 0; j < members[i].length; j++){
+          if (members[i][j] !== ' '){
+            arr.push(members[i][j].toUpperCase());
+            break;
+          }
+        } 
+      }
+    }
+  }
+  return arr.sort().join('');
 };
