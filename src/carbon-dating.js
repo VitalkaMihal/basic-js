589d1e53c9bb17eedeaa9eaf50ed316fe47280f6
VitalkaMihal <vitalimikhalkevich1986@gmail.com>
2021-03-09 23:18:08 +0300

    feat: add age func

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,15 @@
-const CustomError = require("../extensions/custom-error");
 
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function dateSample(sampleActivity)
+{
+  if (typeof sampleActivity !== 'string' || isNaN( parseFloat(sampleActivity)) || sampleActivity === '' || parseFloat(sampleActivity) <= 0 || parseFloat(sampleActivity) > MODERN_ACTIVITY || typeof parseFloat(sampleActivity) !== 'number')
+  {
+    return false;
+  } else {
+    let age = Math.ceil(Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity) ) * HALF_LIFE_PERIOD / 0.693);
+    return age;
+  }
+}
+  
