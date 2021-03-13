7e88316d1a1edf45757fc2d7329f11912a68ac54
VitalkaMihal <vitalimikhalkevich1986@gmail.com>
2021-03-13 19:54:14 +0300

    feat: add repeater full

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,31 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
-  
+module.exports = function repeater( str, options ) {
+  if (!options){return str;}else {
+    if (!options.repeatTimes){
+          options.repeatTimes = 0;
+      }
+      if (options.separator === undefined) {
+        options.separator = '+';
+      }
+      if (options.addition === undefined) {
+        options.addition = '';
+      }
+      if (options.additionRepeatTimes === undefined) {
+        options.additionRepeatTimes = 0;
+      }
+      if (options.additionSeparator === undefined) {
+        options.additionSeparator = '|';
+      }
+      let optEnd = options.addition;
+      for (let i = 2; i <= options.additionRepeatTimes ; i++) {
+        optEnd += options.additionSeparator + options.addition;
+      }
+      let strEnd = str + optEnd;
+      for (let i = 2; i <= options.repeatTimes; i++) {
+        strEnd += options.separator + str + optEnd;
+      }
+     
+      console.log(String(options.addition));
+      return strEnd;
+      }
+    };
+    
